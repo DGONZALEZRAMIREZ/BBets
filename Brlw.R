7acc2412dff0a7d7cf0c86bea7b7d4be0a0b1e74
dgonzalezramirez <dgonzalezramirez@avc.edu>
2021-11-24 06:26:58 +0000

    Added hisstograms

---
+++ Brlw.R
@@ -0,0 +1,15 @@
+Brlw <-function(numClass,odds,countoDo = 637){
+
+days=1:365
+count=countoDo
+repeatn=numClass
+varstor=numeric()
+  brownlow = 0
+  for (i in 1:countoDo) {
+    sam=sample(days,size=numClass,replace = T)
+    y=duplicated(sam)
+    if (sum(y)>0) brownlow=brownlow+1
+    else brownlow = brownlow - odds
+  }
+ return(brownlow)
+}
